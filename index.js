2c21f83e37655b790a7e0c9c5954d2b9bf2bc641
Thiago Jesuino <thiago_jesuino@hotmail.com>
2023-09-19 15:56:00 -0300

    Include form validations

--- index.js
+++ index.js
@@ -10,3 +10,36 @@ function updateCount(type) {
 
   currentStickersCount.value = count;
 }
+
+function validateForm() {
+  const reactCheckbox = document.getElementById("react-checkbox");
+  const vueCheckbox = document.getElementById("vue-checkbox");
+  const angularCheckbox = document.getElementById("angular-checkbox");
+  const stickersQuantity = parseInt(
+    document.getElementById("stickers-count").value
+  );
+  const typeError = document.querySelector(".error-text-sticker-type");
+  const quantityError = document.querySelector(".error-text-sticker-quantity");
+
+  typeError.classList.remove("show");
+  quantityError.classList.remove("show");
+
+  if (
+    !reactCheckbox.checked &&
+    !vueCheckbox.checked &&
+    !angularCheckbox.checked
+  ) {
+    typeError.classList.add("show");
+  }
+
+  if (!stickersQuantity || stickersQuantity <= 0) {
+    quantityError.classList.add("show");
+  }
+}
+
+const formulario = document.getElementById("stickers-form");
+
+formulario.addEventListener("submit", function (e) {
+  e.preventDefault();
+  validateForm();
+});
